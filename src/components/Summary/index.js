7ffb0ce3b534a2299bd988a77665906e9402ecdc
Minh <nguyentrongminh1412@gmail.com>
2022-03-31 14:13:07 +0700

    component summary

--- src/components/Summary/index.js
+++ src/components/Summary/index.js
@@ -1,14 +1,33 @@
-import { Grid } from '@mui/material';
-import React from 'react';
-import LineChart from '../Charts/LineChart';
+import React, { useEffect, useState } from 'react';
+import Grid from '@material-ui/core/Grid';
 
-export default function Summary({ report }) {
+import { getMapDataByCountryId } from '../apis';
+import LineChart from '../Charts/LineChart';
+import HighMaps from '../Charts/HighMaps';
+
+export default function Summary({ countryId, report }) {
+  const [mapData, setMapData] = useState({});
+
+  useEffect(() => {
+    if (countryId) {
+      getMapDataByCountryId(countryId)
+        .then((res) => {
+          setMapData(res);
+        })
+        .catch((err) => console.log({ err }));
+    }
+  }, [countryId]);
+
   return (
-    <Grid container spacing={3}>
-      <Grid item sm={8} xs={12}>
-        <LineChart data={report} />
+    <div style={{ height: '500px', marginTop: 10 }}>
+      <Grid container spacing={3}>
+        <Grid item sm={8} xs={12}>
+          <LineChart data={report} />
+        </Grid>
+        <Grid item sm={4} xs={12}>
+          <HighMaps mapData={mapData} />
+        </Grid>
       </Grid>
-      <Grid item sm={8} xs={12}></Grid>
-    </Grid>
+    </div>
   );
 }
