e2b91d23e0915d5728cae5a1b773e8630c5079c4
Minh <nguyentrongminh1412@gmail.com>
2022-03-31 14:12:49 +0700

    update api

--- src/components/apis/index.js
+++ src/components/apis/index.js
@@ -1,8 +1,17 @@
 import axios from 'axios';
+import moment from 'moment';
 
 export const getCountries = () =>
   axios.get('https://api.covid19api.com/countries');
 
-export const getReportByCountry = (country) =>
-  axios.get(`https://api.covid19api.com/dayone/country/${country}`);
-// axios là 1 promise nên khi gọi cần then
+export const getReportByCountry = (slug) =>
+  axios.get(
+    `https://api.covid19api.com/dayone/country/${slug}?from=2021-01-01T00:00:00&to=${moment()
+      .utc(0)
+      .format()}`
+  );
+
+export const getMapDataByCountryId = (countryId) =>
+  import(
+    `@highcharts/map-collection/countries/${countryId}/${countryId}-all.geo.json`
+  );
