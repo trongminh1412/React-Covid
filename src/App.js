d8ba137ae4f0e812a67df573991cf3c9cc287411
Minh <nguyentrongminh1412@gmail.com>
2022-03-31 08:53:06 +0700

    app.js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,56 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from 'react';
+import { getCountries, getReportByCountry } from './components/apis';
+import CountrySelector from './components/CountrySelector';
+import Highlight from './components/Highlight';
+import Summary from './components/Summary';
 
 function App() {
+  const [countries, setCountries] = useState([]);
+  const [selectedCountryId, setSelectedCountryId] = useState('');
+  const [report, setReport] = useState([]);
+
+  useEffect(() => {
+    getCountries().then((res) => {
+      setCountries(res.data);
+
+      setSelectedCountryId('vn');
+    });
+  }, []);
+
+  const handleOnChange = (e) => {
+    setSelectedCountryId(e.target.value);
+  };
+
+  useEffect(() => {
+    if (selectedCountryId) {
+      const { Slug } = countries.find(
+        (country) => country.ISO2.toLowerCase() === selectedCountryId
+      );
+
+      //call api
+      getReportByCountry(Slug).then((res) => {
+        //xóa dữ liệu data cuối trong array res.data
+        res.data.pop();
+        setReport(res.data);
+      });
+    }
+  }, [countries, selectedCountryId]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <CountrySelector
+        countries={countries}
+        handleOnChange={handleOnChange}
+        value={selectedCountryId}
+      />
+      <Highlight report={report} />
+      <Summary report={report} />
+    </>
   );
 }
 
 export default App;
+
+/**
+ * call api từ 1 functional component cần bọc trong useEffect
+ */
