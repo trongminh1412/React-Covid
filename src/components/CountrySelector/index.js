2a040d275d3082a7f398b499b4fea01d0d2a2d2d
Minh <nguyentrongminh1412@gmail.com>
2022-03-31 14:13:40 +0700

    component country selector

--- src/components/CountrySelector/index.js
+++ src/components/CountrySelector/index.js
@@ -1,29 +1,44 @@
 import React from 'react';
-import {
-  FormControl,
-  FormHelperText,
-  InputLabel,
-  NativeSelect,
-} from '@mui/material';
+import InputLabel from '@material-ui/core/InputLabel';
+import FormHelperText from '@material-ui/core/FormHelperText';
+import FormControl from '@material-ui/core/FormControl';
+import NativeSelect from '@material-ui/core/NativeSelect';
+import { makeStyles } from '@material-ui/core/styles';
 
-export default function CountrySelector({ value, handleOnChange, countries }) {
+const useStyles = makeStyles((theme) => ({
+  formControl: {
+    margin: `${theme.spacing(3)}px 0`,
+    minWidth: 120,
+  },
+}));
+
+export default function CountrySelector({ countries, handleOnChange, value }) {
+  const classes = useStyles();
+
   return (
-    <FormControl>
-      <InputLabel htmlFor="country-selector" shrink></InputLabel>
+    <FormControl className={classes.formControl}>
+      <InputLabel shrink htmlFor='country-selector'>
+        Quốc Gia
+      </InputLabel>
       <NativeSelect
         value={value}
         onChange={handleOnChange}
-        inputProps={{ name: 'country', id: 'country-selector' }}
+        inputProps={{
+          name: 'country',
+          id: 'country-selector',
+        }}
       >
-        {countries?.map((country) => {
-          return (
-            <option value={country.ISO2.toLowerCase()}>
-              {country.Country}
-            </option>
-          );
-        })}
+        {countries.map(({ Country, ISO2 }) => (
+          <option key={ISO2} value={ISO2.toLowerCase()}>
+            {Country}
+          </option>
+        ))}
       </NativeSelect>
       <FormHelperText>Lựa chọn quốc gia</FormHelperText>
     </FormControl>
   );
 }
+
+CountrySelector.defaultProps = {
+  countries: [],
+};
