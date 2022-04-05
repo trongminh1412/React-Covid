75dbf4011b7be430ea0856c5d9bbcccc83aa9067
Minh <nguyentrongminh1412@gmail.com>
2022-04-05 23:54:43 +0700

    update routing

--- src/components/Highlight/index.js
+++ src/components/Highlight/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Grid } from '@material-ui/core';
-import HighlightCard from './HighlightCard';
+import HighlightCard from './highlightCard';
 
 export default function Highlight({ summary }) {
   return (
